e66f747750f6c5205eb74da3ebb05f5188853633
ltvch <HrenHrenov@myrambler.ru>
2018-04-12 09:11:43 +0300

    Make InsertSorted function

--- InsertSorted/Program.cs
+++ InsertSorted/Program.cs
@@ -4,9 +4,33 @@ namespace InsertSorted
 {
     class Program
     {
+        static void InsertSort(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                int cursor = array[i];
+                int k = i;
+
+                while (i > 0 && cursor < array[k - 1])
+                {
+                    array[k] = array[k - 1];
+                    k--;
+                }
+                array[k] = cursor;
+            }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] arr = new[] { 1, 4, 5, 7, 7, 8, 2, 3, 7 };
+
+            InsertSort(arr);
+
+            foreach (int item in arr)
+            {
+                Console.Write(item);
+                Console.Write(" ");
+            }
+            Console.Read();
         }
     }
 }
